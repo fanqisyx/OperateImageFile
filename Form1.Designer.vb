de7c824f2a473d55f912f4b2ea6e184903af7755
fanqi <fanqi@DESKTOP-EO60T56>
2023-05-26 17:11:22 +0800

    添加项目文件。

---
+++ Form1.Designer.vb
@@ -0,0 +1,341 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtInputDirectory = New System.Windows.Forms.TextBox()
+        Me.txtOutputDirectory = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.trackBarQuality = New System.Windows.Forms.TrackBar()
+        Me.lblQuality = New System.Windows.Forms.Label()
+        Me.lblProgress = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtCropX = New System.Windows.Forms.TextBox()
+        Me.txtCropY = New System.Windows.Forms.TextBox()
+        Me.txtCropWidth = New System.Windows.Forms.TextBox()
+        Me.txtCropHeight = New System.Windows.Forms.TextBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        CType(Me.trackBarQuality, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtInputDirectory
+        '
+        Me.txtInputDirectory.Location = New System.Drawing.Point(88, 12)
+        Me.txtInputDirectory.Name = "txtInputDirectory"
+        Me.txtInputDirectory.Size = New System.Drawing.Size(621, 21)
+        Me.txtInputDirectory.TabIndex = 0
+        '
+        'txtOutputDirectory
+        '
+        Me.txtOutputDirectory.Location = New System.Drawing.Point(88, 40)
+        Me.txtOutputDirectory.Name = "txtOutputDirectory"
+        Me.txtOutputDirectory.Size = New System.Drawing.Size(621, 21)
+        Me.txtOutputDirectory.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(613, 31)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(96, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "BMP 转 JPG"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(88, 68)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(621, 23)
+        Me.ProgressBar1.TabIndex = 3
+        Me.ProgressBar1.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(17, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(65, 12)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "原图像目录"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(5, 43)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 12)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "目标图像目录"
+        '
+        'trackBarQuality
+        '
+        Me.trackBarQuality.Location = New System.Drawing.Point(19, 20)
+        Me.trackBarQuality.Name = "trackBarQuality"
+        Me.trackBarQuality.Size = New System.Drawing.Size(401, 45)
+        Me.trackBarQuality.TabIndex = 6
+        '
+        'lblQuality
+        '
+        Me.lblQuality.AutoSize = True
+        Me.lblQuality.Location = New System.Drawing.Point(427, 36)
+        Me.lblQuality.Name = "lblQuality"
+        Me.lblQuality.Size = New System.Drawing.Size(41, 12)
+        Me.lblQuality.TabIndex = 7
+        Me.lblQuality.Text = "Label3"
+        '
+        'lblProgress
+        '
+        Me.lblProgress.AutoSize = True
+        Me.lblProgress.Location = New System.Drawing.Point(360, 74)
+        Me.lblProgress.Name = "lblProgress"
+        Me.lblProgress.Size = New System.Drawing.Size(41, 12)
+        Me.lblProgress.TabIndex = 8
+        Me.lblProgress.Text = "Label3"
+        Me.lblProgress.Visible = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(613, 40)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(96, 23)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "BMP图形裁剪"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.SystemColors.Control
+        Me.GroupBox1.Controls.Add(Me.GroupBox2)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GroupBox1.Location = New System.Drawing.Point(3, 3)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(713, 76)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "图像裁切"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.txtCropHeight)
+        Me.GroupBox2.Controls.Add(Me.txtCropWidth)
+        Me.GroupBox2.Controls.Add(Me.txtCropY)
+        Me.GroupBox2.Controls.Add(Me.txtCropX)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Dock = System.Windows.Forms.DockStyle.Left
+        Me.GroupBox2.Location = New System.Drawing.Point(3, 17)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(515, 56)
+        Me.GroupBox2.TabIndex = 0
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "裁切区域"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(10, 29)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(35, 12)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "起始X"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(132, 29)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(35, 12)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "起始Y"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(382, 29)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(41, 12)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "图像高"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(254, 29)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(41, 12)
+        Me.Label6.TabIndex = 2
+        Me.Label6.Text = "图形宽"
+        '
+        'txtCropX
+        '
+        Me.txtCropX.Location = New System.Drawing.Point(51, 25)
+        Me.txtCropX.Name = "txtCropX"
+        Me.txtCropX.Size = New System.Drawing.Size(75, 21)
+        Me.txtCropX.TabIndex = 4
+        Me.txtCropX.Text = "0"
+        '
+        'txtCropY
+        '
+        Me.txtCropY.Location = New System.Drawing.Point(173, 25)
+        Me.txtCropY.Name = "txtCropY"
+        Me.txtCropY.Size = New System.Drawing.Size(75, 21)
+        Me.txtCropY.TabIndex = 5
+        Me.txtCropY.Text = "0"
+        '
+        'txtCropWidth
+        '
+        Me.txtCropWidth.Location = New System.Drawing.Point(301, 25)
+        Me.txtCropWidth.Name = "txtCropWidth"
+        Me.txtCropWidth.Size = New System.Drawing.Size(75, 21)
+        Me.txtCropWidth.TabIndex = 6
+        Me.txtCropWidth.Text = "640"
+        '
+        'txtCropHeight
+        '
+        Me.txtCropHeight.Location = New System.Drawing.Point(429, 25)
+        Me.txtCropHeight.Name = "txtCropHeight"
+        Me.txtCropHeight.Size = New System.Drawing.Size(75, 21)
+        Me.txtCropHeight.TabIndex = 7
+        Me.txtCropHeight.Text = "480"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.BackColor = System.Drawing.SystemColors.Control
+        Me.GroupBox3.Controls.Add(Me.trackBarQuality)
+        Me.GroupBox3.Controls.Add(Me.Button1)
+        Me.GroupBox3.Controls.Add(Me.lblQuality)
+        Me.GroupBox3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GroupBox3.Location = New System.Drawing.Point(3, 3)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(713, 76)
+        Me.GroupBox3.TabIndex = 11
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "BMP转JPG"
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.TabControl1.Location = New System.Drawing.Point(0, 104)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(727, 108)
+        Me.TabControl1.TabIndex = 12
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.GroupBox3)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(719, 82)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "BMP转JPG"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.GroupBox1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(719, 82)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "图像裁切"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(727, 212)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Controls.Add(Me.lblProgress)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.txtOutputDirectory)
+        Me.Controls.Add(Me.txtInputDirectory)
+        Me.Name = "Form1"
+        Me.Text = "图像简单处理工具箱"
+        CType(Me.trackBarQuality, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtInputDirectory As TextBox
+    Friend WithEvents txtOutputDirectory As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents trackBarQuality As TrackBar
+    Friend WithEvents lblQuality As Label
+    Friend WithEvents lblProgress As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents txtCropHeight As TextBox
+    Friend WithEvents txtCropWidth As TextBox
+    Friend WithEvents txtCropY As TextBox
+    Friend WithEvents txtCropX As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+End Class
---
+++ Form1.vb
@@ -0,0 +1,179 @@
+﻿Imports System.IO
+Imports System.Drawing
+Imports System.Drawing.Imaging
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim inputDirectory As String = txtInputDirectory.Text.Trim()
+        Dim outputDirectory As String = txtOutputDirectory.Text.Trim()
+
+        ' 保存当前填写的目录值到应用程序设置
+        My.Settings.InputDirectory = txtInputDirectory.Text.Trim()
+        My.Settings.OutputDirectory = txtOutputDirectory.Text.Trim()
+        My.Settings.qualityPercentage = trackBarQuality.Value
+        My.Settings.Save()
+
+        ' 检查输入目录是否存在
+        If Not Directory.Exists(inputDirectory) Then
+            MessageBox.Show("输入目录不存在。")
+            Return
+        End If
+
+        ' 检查输出目录是否存在，如果不存在则创建
+        If Not Directory.Exists(outputDirectory) Then
+            Directory.CreateDirectory(outputDirectory)
+        End If
+
+        ' 获取输入目录下的所有BMP文件
+        Dim bmpFiles As String() = Directory.GetFiles(inputDirectory, "*.bmp")
+
+        ' 显示进度条
+        ProgressBar1.Minimum = 0
+        ProgressBar1.Maximum = bmpFiles.Length
+        ProgressBar1.Value = 0
+        ProgressBar1.Visible = True
+        lblProgress.Visible = True
+
+        Dim qualityPercentage As Integer = trackBarQuality.Value
+
+        ' 启动后台任务
+        Task.Run(Sub()
+                     For i As Integer = 0 To bmpFiles.Length - 1
+                         Dim bmpFile As String = bmpFiles(i)
+                         Dim outputFileName As String = Path.GetFileNameWithoutExtension(bmpFile) & ".jpg"
+                         Dim outputPath As String = Path.Combine(outputDirectory, outputFileName)
+
+                         Using bmp As New Bitmap(bmpFile)
+                             ' 设置JPEG质量
+                             Dim encoderParams As New EncoderParameters(1)
+                             Dim encoderParam As New EncoderParameter(Encoder.Quality, qualityPercentage)
+                             encoderParams.Param(0) = encoderParam
+
+                             ' 获取JPEG编码器
+                             Dim jpegCodecInfo As ImageCodecInfo = ImageCodecInfo.GetImageEncoders().FirstOrDefault(Function(codec) codec.FormatID = ImageFormat.Jpeg.Guid)
+
+                             ' 保存图像为JPEG
+                             bmp.Save(outputPath, jpegCodecInfo, encoderParams)
+                         End Using
+
+
+                         ' 更新进度条和已完成比例
+                         Dim progress As Double = (i + 1) / bmpFiles.Length * 100
+                         Me.Invoke(Sub()
+                                       ProgressBar1.Value = i + 1
+                                       lblProgress.Text = $"已完成: {progress:F2}%"
+                                   End Sub)
+                     Next
+
+                     ' 使用委托在任务完成后执行指定的操作
+                     Me.Invoke(Sub()
+                                   ProgressBar1.Visible = False
+                                   lblProgress.Visible = False
+                                   MessageBox.Show("转换完成。")
+                               End Sub)
+                 End Sub)
+    End Sub
+    Private Function GetEncoderInfo(format As ImageFormat) As ImageCodecInfo
+        Dim codecs As ImageCodecInfo() = ImageCodecInfo.GetImageEncoders()
+        For Each codec As ImageCodecInfo In codecs
+            If codec.FormatID = format.Guid Then
+                Return codec
+            End If
+        Next
+        Return Nothing
+    End Function
+
+    ' 质量调节滑动条值改变事件
+    Private Sub trackBarQuality_ValueChanged(sender As Object, e As EventArgs) Handles trackBarQuality.ValueChanged
+        lblQuality.Text = $"Quality: {trackBarQuality.Value}%"
+    End Sub
+
+    ' 窗体加载事件
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        trackBarQuality.Minimum = 30
+        trackBarQuality.Maximum = 100
+        trackBarQuality.Value = My.Settings.qualityPercentage
+        lblQuality.Text = $"Quality: {trackBarQuality.Value}%"
+
+        lblProgress.BackColor = Color.Transparent
+
+        ' 检查并设置输入目录的初始值
+        If Not String.IsNullOrEmpty(My.Settings.InputDirectory) Then
+            txtInputDirectory.Text = My.Settings.InputDirectory
+        End If
+
+        ' 检查并设置输出目录的初始值
+        If Not String.IsNullOrEmpty(My.Settings.OutputDirectory) Then
+            txtOutputDirectory.Text = My.Settings.OutputDirectory
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' 获取输入目录和输出目录
+        Dim inputDirectory As String = txtInputDirectory.Text.Trim()
+        Dim outputDirectory As String = txtOutputDirectory.Text.Trim()
+
+        ' 检查输入目录是否存在
+        If Not Directory.Exists(inputDirectory) Then
+            MessageBox.Show("输入目录不存在。")
+            Return
+        End If
+
+        ' 检查输出目录是否存在，如果不存在则创建
+        If Not Directory.Exists(outputDirectory) Then
+            Directory.CreateDirectory(outputDirectory)
+        End If
+
+        ' 获取选定的裁剪区域
+        Dim x As Integer = CInt(txtCropX.Text.Trim())
+        Dim y As Integer = CInt(txtCropY.Text.Trim())
+        Dim width As Integer = CInt(txtCropWidth.Text.Trim())
+        Dim height As Integer = CInt(txtCropHeight.Text.Trim())
+
+        ' 获取输入目录下的所有BMP文件
+        'Dim imageFiles As String() = Directory.GetFiles(inputDirectory, "*.*").Where(Function(file) file.ToLower().EndsWith(".jpg") OrElse file.ToLower().EndsWith(".png") OrElse file.ToLower().EndsWith(".bmp")).ToArray()
+        Dim bmpFiles As String() = Directory.GetFiles(inputDirectory, "*.bmp")
+
+        ' 显示进度条
+        ProgressBar1.Minimum = 0
+        ProgressBar1.Maximum = bmpFiles.Length
+        ProgressBar1.Value = 0
+        ProgressBar1.Visible = True
+        lblProgress.Visible = True
+
+        ' 启动后台任务
+        Task.Run(Sub()
+                     For i As Integer = 0 To bmpFiles.Length - 1
+                         Dim bmpFile As String = bmpFiles(i)
+                         Dim outputFileName As String = Path.GetFileNameWithoutExtension(bmpFile) & "_cropped.bmp"
+                         Dim outputPath As String = Path.Combine(outputDirectory, outputFileName)
+
+                         Using bmp As New Bitmap(bmpFile)
+                             ' 创建一个新的Bitmap对象，用于存储裁剪后的图像
+                             Dim croppedBmp As New Bitmap(width, height)
+
+                             ' 创建Graphics对象，并将其绘制到裁剪后的图像上
+                             Using g As Graphics = Graphics.FromImage(croppedBmp)
+                                 g.DrawImage(bmp, New Rectangle(0, 0, width, height), New Rectangle(x, y, width, height), GraphicsUnit.Pixel)
+                             End Using
+
+                             ' 保存裁剪后的图像为BMP格式
+                             croppedBmp.Save(outputPath, ImageFormat.Bmp)
+                         End Using
+
+
+                         ' 更新进度条和已完成比例
+                         Dim progress As Double = (i + 1) / bmpFiles.Length * 100
+                         Me.Invoke(Sub()
+                                       ProgressBar1.Value = i + 1
+                                       lblProgress.Text = $"已完成: {progress:F2}%"
+                                   End Sub)
+                     Next
+
+                     ' 使用委托在任务完成后执行指定的操作
+                     Me.Invoke(Sub()
+                                   ProgressBar1.Visible = False
+                                   MessageBox.Show("裁剪完成。")
+                               End Sub)
+                 End Sub)
+    End Sub
+End Class
